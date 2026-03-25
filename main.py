f240026327761fe384c1162a5d495e499e7267be
aristizabal7m <s.aristizabal3@utp.edu.co>
2026-03-24 20:14:46 -0500

    Se añaden dependencias

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ load_dotenv()
 
 @app.get("/llm/{prompt}")
 async def read_root(prompt):
-from google import genai
-# The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client()
-response = client.models.generate_content(
-model="gemini-3-flash-preview", contents=prompt
-)
-print(response.text)
-return {"Respuesta": response.text}
+
+    from google import genai
+    # The client gets the API key from the environment variable `GEMINI_API_KEY`.
+    client = genai.Client()
+    response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents=prompt
+    )
+    print(response.text)
+    return {"Respuesta": response.text}
